ec0159be8a1f3c35eac0d133d2bb311e02e41f00
byionline <10815566+byionline@users.noreply.github.com>
2021-09-20 11:19:06 +0530

    Added Navigation Drawer

--- src/components/navBar/navItems.js
+++ src/components/navBar/navItems.js
@@ -1,5 +1,9 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import tw from "twin.macro";
+import { slide as Menu } from "react-burger-menu";
+import { useMediaQuery } from "react-responsive";
+import menuStyles from "./menuStyles";
+import { SCREENS } from "../responsive";
 const ListContainer = styled.ul`
   ${tw`
     flex
@@ -20,22 +24,52 @@ const NavItem = styled.li`
     ease-in-out
     hover:text-gray-700
   `};
+  ${({ menu }) =>
+    menu &&
+    css`
+      ${tw`
+      text-white
+      text-xl
+      mb-3
+      focus:text-white
+    `};
+    `};
 `;
 export function NavItems() {
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+  if (isMobile)
     return (
+      <Menu right styles={menuStyles}>
         <ListContainer>
-            <NavItem>
-                <a href="#">Home</a>
-            </NavItem>
-            <NavItem>
-                <a href="#">Cars</a>
-            </NavItem>
-            <NavItem>
-                <a href="#">Services</a>
-            </NavItem>
-            <NavItem>
-                <a href="#">Contact Us</a>
-            </NavItem>
+          <NavItem menu>
+            <a href="#">Home</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Cars</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Services</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Contact Us</a>
+          </NavItem>
         </ListContainer>
+      </Menu>
     );
+  return (
+    <ListContainer>
+      <NavItem>
+        <a href="#">Home</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Cars</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Services</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Contact Us</a>
+      </NavItem>
+    </ListContainer>
+  );
 }
